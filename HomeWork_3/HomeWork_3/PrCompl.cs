d41aa909960c84874397ddffcb12faf27d2bb75c
Astori191 <gdmreol@gmail.com>
2021-02-28 00:47:09 +0300

    Refactoring

--- HomeWork_3/HomeWork_3/PrCompl.cs
+++ HomeWork_3/HomeWork_3/PrCompl.cs
@@ -13,27 +13,27 @@ namespace HomeWork_3
             Console.WriteLine($"z1: {z1.Print()}");
             Console.WriteLine($"z2: {z2.Print()}");
 
-            Complex addRes = Complex.Addition(z1, z2);
-            //Console.WriteLine($"Sum: {addRes.Print()}");
-
-            Complex subRes = Complex.Subtraction(z1, z2);
-            //Console.WriteLine($"Diff: {subRes.Print()}");
-
-            Complex multRes = Complex.Multiplication(z1, z2);
-            //Console.WriteLine($"Mult: {multRes.Print()}");
-
             string userChoice = SelectOperation();
 
             switch (userChoice)
             {
                 case "+":
-                    Console.WriteLine($"Sum: {addRes.Print()}");
+                    {
+                        Complex addRes = Complex.Addition(z1, z2);
+                        Console.WriteLine($"Sum: {addRes.Print()}");
+                    }
                     break;
                 case "-":
-                    Console.WriteLine($"Diff: {subRes.Print()}");
+                    { 
+                        Complex subRes = Complex.Subtraction(z1, z2);
+                        Console.WriteLine($"Diff: {subRes.Print()}");
+                    }
                     break;
                 case "*":
-                    Console.WriteLine($"Mult: {multRes.Print()}");
+                    {
+                        Complex multRes = Complex.Multiplication(z1, z2);
+                        Console.WriteLine($"Mult: {multRes.Print()}");
+                    }
                     break;
                 default:
                     Console.WriteLine("Something gone wrong!");
@@ -41,18 +41,6 @@ namespace HomeWork_3
             }
 
             Console.WriteLine();
-
-            Fraction f1 = new Fraction(3, 7);
-            Fraction f2 = new Fraction(2, 7);
-
-            Console.WriteLine($"f1: {f1.PrintFraction()}");
-            Console.WriteLine($"f2: {f2.PrintFraction()}");
-
-            Fraction frAddRes = Fraction.Addition(f1, f2);
-            Console.WriteLine($"Sum: {frAddRes.PrintFraction()}");
-
-
-
 
         }
         static string SelectOperation()
