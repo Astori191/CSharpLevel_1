eab729e7690f8fda91fa3fc9b60718b493ae0c9b
Astori191 <gdmreol@gmail.com>
2021-03-03 15:11:27 +0300

    Class Account. Task4.

---
+++ HomeWork_4/LogPas/Account.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace LogPas
+{
+    //4. Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив.
+    //Создайте структуру Account, содержащую Login и Password.
+
+    class Account
+    {
+        private string login;
+        private string password;
+
+        public Account(string l, string p)
+        {
+            this.login = l;
+            this.password = p;
+        }
+        /// <summary>
+        /// Метод получения массива с логином[0] и паролем[1] из файла
+        /// </summary>
+        /// <returns>Массив</returns>
+        public string[] GetArrayFromFile()
+        {
+            string[] array = new string[0];
+            try
+            {
+                string path = @"C:\temp\Data.txt";
+                int rows = File.ReadAllLines(path).Length;
+                StreamReader sr = new StreamReader(path);
+                array = new string[rows];
+                while (!sr.EndOfStream)
+                {
+                    for (int i = 0; i < rows; i++)
+                    {
+                        array[i] = sr.ReadLine();
+                    }
+                }
+                sr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return array;
+        }
+        /// <summary>
+        /// Метод проверки пользовательского ввода с эталонными значениями
+        /// </summary>
+        /// <param name="account">экземпляр класса Account с параметрами login password</param>
+        /// <param name="existing">массив значений из файла</param>
+        /// <returns></returns>
+        public bool Authentication(Account account, string[] existing)
+        {
+            bool isReal = false;
+            int len = existing.Length - 1;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (existing[i] == account.login && existing[i + 1] == account.password)
+                {
+                    isReal = true;
+                    Console.WriteLine($"Your login: *{existing[i]}* and password: *{existing[i + 1]}* accepted!");
+                }
+                else
+                {
+                    isReal = false;
+                    Console.WriteLine("Incorrect login or password!");
+                }
+
+            }
+
+            return isReal;
+        }
+
+    }
+}
+
+
---
+++ HomeWork_4/LogPas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace LogPas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxTry = 3;
+
+            while (maxTry != 0)
+            {
+                Console.Write("Enter login: ");
+                string userLogin = Console.ReadLine();
+                Console.Write("Enter password: ");
+                string userPassword = Console.ReadLine();
+
+                Account account = new Account(userLogin, userPassword);
+
+                string[] correctLoginAndPassword = account.GetArrayFromFile();
+
+                if (account.Authentication(account, correctLoginAndPassword) == true) break;                  
+                maxTry--;
+            }
+        }
+    }
+}
