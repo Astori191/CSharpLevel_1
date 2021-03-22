bb5da54b14e3d32c9951e34c423e04088d378109
Astori191 <gdmreol@gmail.com>
2021-03-22 20:16:44 +0300

    Commit

---
+++ HomeWork_8/Ref/NumUpDown/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumUpDown
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = numericUpDown1.Value.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                numericUpDown1.Value = decimal.Parse(textBox1.Text); 
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Not a number!");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("Out of range");
+            }
+        }
+    }
+}
---
+++ HomeWork_8/Ref/Ref/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+// С помощью рефлексии выведите все свойства структуры DateTime
+
+namespace Ref
+{
+    class Program
+    {
+        static void Main()
+        {
+            Type type = typeof(DateTime);
+            foreach (var prop in type.GetProperties())
+            {
+                Console.WriteLine(prop.Name);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
