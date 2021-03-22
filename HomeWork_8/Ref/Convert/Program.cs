a23f25cc56592997f276c3b46fa971d8faf584e0
Astori191 <gdmreol@gmail.com>
2021-03-22 20:24:28 +0300

    Converter

---
+++ HomeWork_8/Ref/Convert/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Convert
+{
+    class Program
+    {
+        static void Converter(string fileNameOpen, string fileNameSave)
+        {
+            string[] lines = File.ReadAllLines(fileNameOpen);
+            string[] headers = { "Name", "Surname", "University", "Faculty", "Department", "Age", "Course", "Group", "City" };
+
+            var xml = new XElement("Students",
+               lines.Where((line, index) => index > 0).Select(line => new XElement("StudentIndo",
+                  line.Split(';').Select((column, index) => new XElement(headers[index], column)))));
+
+            xml.Save(fileNameSave);
+        }
+
+        static void Main()
+        {
+            Converter(@"C:\temp\Students.csv", @"C:\temp\St.xml");
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+    }
+}
